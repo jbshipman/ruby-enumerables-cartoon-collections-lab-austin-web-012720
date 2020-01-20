6f35b125acca94a3e1d06b061bec4f6f26f7785e
James Shipman <jbshipman@icloud.com>
2020-01-20 15:33:01 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,6 +12,7 @@ def long_planeteer_calls(array)
   array.any? { |word| word.length > 4 }
 end
 
-def find_the_cheese
+def find_the_cheese(ingredient_list)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  ingredient_list.include {
 end
